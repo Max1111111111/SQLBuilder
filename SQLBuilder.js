8c68fbe5e580fa724ae0a5f500707d76001259a8
MaxZabara@gmail.com <MaxZabara@gmail.com>
2020-06-17 17:43:36 +0300

    Exp

--- SQLBuilder.js
+++ SQLBuilder.js
@@ -1,3 +1,29 @@
 class SQLBuilder {
+
+    constructor (tableName, alias) {
+        this.tableName = tableName || ''
+        this.alias = alias || '' 
+        this.fields = []
+        this.conditions = []
+    }
+
+    setTableName (tableName, alias) {
+        this.tableName = tableName
+        this.alias = alias  
+    }
+
+    addField (fieldName, alias) {
+        this.fields.push( alias ? `${alias}.${fieldName}` : fieldName )        
+    }
     
+    getSQL() {
+        return `SELECT ${this.fields.join(', ')} \nFROM ${this.tableName}` 
+    }  
 }
+
+
+let sql = new SQLBuilder('companies', 'C')
+sql.addField('id', 'C')
+sql.addField('title', 'C')
+//sql.tableName = 'XC'
+console.log(sql.getSQL())
