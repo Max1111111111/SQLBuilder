62611d89402472486a67aca909296531bba90814
MaxZabara@gmail.com <MaxZabara@gmail.com>
2020-06-18 18:02:56 +0300

    Exp

--- SQLBuilder.js
+++ SQLBuilder.js
@@ -5,6 +5,9 @@ class SQLBuilder {
         this.alias = alias || '' 
         this.fields = []
         this.conditions = []
+        this.group = []
+        this.order = []
+
     }
 
     setTableName (tableName, alias) {
@@ -15,9 +18,26 @@ class SQLBuilder {
     addField (fieldName, alias) {
         this.fields.push( alias ? `${alias}.${fieldName}` : fieldName )        
     }
-    
+
+    addConditionAND(leftOperand, condition, rightOperand) {
+        this.conditions.push( ` AND ${leftOperand || '' } ${condition || '' } ${rightOperand || '' }`.trim() )                     
+    }
+
+    addConditionOR(leftOperand, condition, rightOperand) {
+        this.conditions.push( ` OR ${leftOperand || '' } ${condition || '' } ${rightOperand || '' }`.trim() )                     
+    }
+
+    addGroupBy (fieldName, alias) {
+        this.group.push( alias ? `${alias}.${fieldName}` : fieldName )        
+    }
+
+    addOrderbyBy (fieldName, order) {
+        this.order.push( alias ? `${fieldName} ${order}` : fieldName )        
+    }
+
     getSQL() {
-        return `SELECT ${this.fields.join(', ')} \nFROM ${this.tableName}` 
+        return `SELECT ${this.fields.join(', ')} \nFROM ${this.tableName} \nWHERE (1=1) ${this.conditions.join(' ')}` + 
+        `\nGROUP BY ${this.group.join(', ')}`  
     }  
 }
 
@@ -25,5 +45,8 @@ class SQLBuilder {
 let sql = new SQLBuilder('companies', 'C')
 sql.addField('id', 'C')
 sql.addField('title', 'C')
+sql.addConditionAND('C.title', '=', '$1')
+sql.addConditionOR('id <> 10')
+//sql.#addFields1()
 //sql.tableName = 'XC'
 console.log(sql.getSQL())
