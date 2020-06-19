dd19afd74138afba34efadd27685b98ef55087d8
MaxZabara@gmail.com <MaxZabara@gmail.com>
2020-06-19 12:57:08 +0300

    Exp

--- SQLBuilder.js
+++ SQLBuilder.js
@@ -7,6 +7,7 @@ class SQLBuilder {
         this.conditions = []
         this.group = []
         this.order = []
+        this.joins = []
 
     }
 
@@ -31,13 +32,30 @@ class SQLBuilder {
         this.group.push( alias ? `${alias}.${fieldName}` : fieldName )        
     }
 
-    addOrderbyBy (fieldName, order) {
-        this.order.push( alias ? `${fieldName} ${order}` : fieldName )        
+    addOrderBy (fieldName, order) {
+        this.order.push( order ? `${fieldName} ${order}` : fieldName )        
     }
 
+    addJoin (tableName, alias, leftOperand, rightOperand, condition, joinType) {
+        this.joins.push( `${joinType || ''} JOIN ${tableName} AS ${alias} ON ` + 
+            (rightOperand ? `${leftOperand} ${condition ? condition : ' = '} ${rightOperand}` : leftOperand  ))        
+    }
+
+    addLeftJoin(tableName, alias, leftOperand, rightOperand, condition) {
+        this.addJoin (tableName, alias, leftOperand, rightOperand, condition, "LEFT")
+    }
+
+    addRightJoin(tableName, alias, leftOperand, rightOperand, condition) {
+        this.addJoin (tableName, alias, leftOperand, rightOperand, condition, "RIGHT")
+    }
+
+
     getSQL() {
-        return `SELECT ${this.fields.join(', ')} \nFROM ${this.tableName} \nWHERE (1=1) ${this.conditions.join(' ')}` + 
-        `\nGROUP BY ${this.group.join(', ')}`  
+        return `SELECT ${this.fields.join(', ')} \nFROM ${this.tableName}` +
+        ` \nWHERE (1=1) ${this.conditions.join(' ')}` +  
+        (this.joins.length > 0 ?`\n ${this.joins.join('\n')}` : '') + 
+        (this.group.length > 0 ?`\nGROUP BY ${this.group.join(', ')}` : '') + 
+        (this.order.length > 0 ?`\nORDER BY ${this.order.join(', ')}` : '')  
     }  
 }
 
@@ -47,6 +65,10 @@ sql.addField('id', 'C')
 sql.addField('title', 'C')
 sql.addConditionAND('C.title', '=', '$1')
 sql.addConditionOR('id <> 10')
+sql.addGroupBy('title', 'C')
+sql.addOrderBy('title', 'ASC')
+sql.addLeftJoin('items', 'i', 'c.id', 'i.companies')
+sql.addJoin('items', 'i', 'c.id', 'i.companies')
 //sql.#addFields1()
 //sql.tableName = 'XC'
 console.log(sql.getSQL())
